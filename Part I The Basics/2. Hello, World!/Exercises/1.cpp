0f7f7c373dcdfc3700f4332d353a808e13771911
furkanicer <furkanicertez@gmail.com>
2019-03-06 20:02:29 +0300

    .

---
+++ Part I The Basics/2. Hello, World!/Exercises/1.cpp
@@ -0,0 +1,17 @@
+/* 
+
+1. Change the program to output the two lines
+Hello, programming!
+Here we go!
+
+*/
+
+#include "std_lib_facilities.h"
+
+int main() {
+	cout << "Hello, programming!n";
+    cout << "Here we go!";
+	keep_window_open();
+	
+	return 0;
+}
---
+++ Part I The Basics/2. Hello, World!/Exercises/2, 3, 4.cpp
@@ -0,0 +1,51 @@
+#include "std_lib_facilities.h"
+
+int main() {
+	cout << "Find the door handle, twist that.";
+    cout << "Good, now you opened the door.";
+    cout << "Five steps forward.";
+    cout << "Great, you arrived to stairs.";
+    cout << "Lift your legs to climb up the stairs.";
+    cout << "Repeat that until there is nothing to climb up.";
+    cout << "Look left, you are seeing another door.";
+    cout << "Now again, find it's handle and twist it.";
+    cout << "Wonderful, you have arrived to bathroom!";
+	keep_window_open();
+	
+	return 0;
+}
+
+
+/*
+2.Expanding on what you have learned, write a program that lists the instructions for a computer
+o find the upstairs bathroom, discussed in §2.1. Can you think of any more steps that a person would
+assume, but that a computer would not? Add them to your list. This is a good start in “thinking like a
+computer.” Warning: For most people, “go to the bathroom” is a perfectly adequate instruction. For someone
+with no experience with houses or bathrooms (imagine a stone-age person, somehow transported into your dining room)
+the list of necessary instructions could be very long. Please don’t use more than a page. For the benefit of
+the reader, you may add a short description of the layout of the house you are imagining.
+*/
+
+// Exercise 3 and exercise 4 are pretty much same with exercise 2. So i wont create code for them, if you want to do it you gotta write lots of cout s :D
+
+/*
+3. Write a description of how to get from the front door of your dorm room, apartment, house, whatever,
+to the door of your classroom (assuming you are attending some school; if you are not, pick another target).
+Have a friend try to follow the instructions and annotate them with improvements as he or she goes along.
+To keep friends, it may be a good idea to “field test” those instructions before giving them to a friend.
+*/
+
+/*
+4. Find a good cookbook. Read the instructions for baking blueberry muffins (if you are in a country where “blueberry muffins” is a strange,
+exotic dish, use a more familiar dish instead). Please note that with a bit of help and instruction, most of the people in the world can bake
+delicious blueberry muffins. It is not considered advanced or difficult fine cooking. However, for the author, few exercises in this book are
+as difficult as this one. It is amazing what you can do with a bit of practice.
+• Rewrite those instructions so that each individual action is in its own numbered paragraph. Be careful to list all ingredients
+and all kitchen utensils used at each step. Be careful about crucial details, such as the desired oven temperature, preheating the oven,
+the preparation of the muffin pan, the way to time the cooking, and the need to protect your hands when removing the muffins from the oven.
+• Consider those instructions from the point of view of a cooking novice (if you are not one, get help from a friend who does not know how to cook).
+Fill in the steps that the book’s author (almost certainly an experienced cook) left out for being obvious.
+• Build a glossary of terms used. (What’s a muffin pan? What does preheating do? What do you mean by “oven”?)
+• Now bake some muffins and enjoy your results.
+*/
+
